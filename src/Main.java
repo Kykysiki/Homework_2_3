d73de9e15bd4fe7fedf1e3422f9e020d724a96c1
Глеб <bul.gleb@gmail.com>
2022-08-25 11:15:14 +0400

    Homework_2_3 (Задача 8)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var boxer_1 = 78.2;
-        var boxer_2 = 82.7;
-        var weight = boxer_2 - boxer_1;
-        System.out.println("Разница между весами бойцов " + weight + " кг.");
-        var difference = boxer_2 % boxer_1;
-        System.out.println("Разница между весами бойцов " + difference + " кг.");
+        var WorkingHours = 640;
+        var Work = 8;
+        var worker = WorkingHours / Work;
+        System.out.println("Всего работников в компании - " + worker + " человек.");
+        var worker2 = worker + 94;
+        System.out.println("Если в компании работает " + worker2 + " человек,то всего " + WorkingHours/worker2 + " часа работы может быть поделено между сотрудниками");
     }
 }
