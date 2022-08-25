ad8d96f26ac04b62120e4055cbbad8a38456cdbb
Глеб <bul.gleb@gmail.com>
2022-08-25 11:00:47 +0400

    Homework_2_3 (Задача 7)

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var boxer_1 = 78.2;
         var boxer_2 = 82.7;
-        var weight = boxer_1 + boxer_2;
-        System.out.println("Общий вес двух бойцов " + weight + " кг.");
-        var difference = boxer_2 - boxer_1;
+        var weight = boxer_2 - boxer_1;
+        System.out.println("Разница между весами бойцов " + weight + " кг.");
+        var difference = boxer_2 % boxer_1;
         System.out.println("Разница между весами бойцов " + difference + " кг.");
     }
 }
