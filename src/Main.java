23e692fbdf56a9cee9b27485b09f63434913a406
Глеб <bul.gleb@gmail.com>
2022-08-25 16:38:32 +0400

    Homework_2_3 - Урок 2 (Задача 3)

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        byte ice = 67;
-        System.out.println("Значение переменной с типом byte - " + ice);
-        short fish = 569;
-        System.out.println("Значение переменной с типом short - " + fish);
-        short cars = 27897;
-        System.out.println("Значение переменной с типом short - " + cars);
-        short watch = -159;
-        System.out.println("Значение переменной с типом short - " + watch);
-        long stars = 987_678_965_549L;
-        System.out.println("Значение переменной с типом long - " + stars);
-        float water = 27.12f;
-        System.out.println("Значение переменной с типом float - " + water);
-        float milk = 2.786f;
-        System.out.println("Значение переменной с типом float - " + milk);
-        boolean test = false;
-        System.out.println("Значение переменной с типом boolean - " + test);
+        byte ludmila = 23;
+        byte anna = 27;
+        byte catherine = 30;
+        int students = ludmila + anna + catherine;
+        short paper = 480;
+        paper /= students;
+        System.out.println("На каждого ученика рассчитано "+ paper +" листов бумаги");
     }
 }
