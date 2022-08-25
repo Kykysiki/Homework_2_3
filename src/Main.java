2505e608932a2a740160891d480c4c673382739a
Глеб <bul.gleb@gmail.com>
2022-08-25 10:53:25 +0400

    Homework_2_3 (Задача 6)

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var boxer_1 = 78.2;
+        var boxer_2 = 82.7;
+        var weight = boxer_1 + boxer_2;
+        System.out.println("Общий вес двух бойцов " + weight + " кг.");
+        var difference = boxer_2 - boxer_1;
+        System.out.println("Разница между весами бойцов " + difference + " кг.");
     }
 }
